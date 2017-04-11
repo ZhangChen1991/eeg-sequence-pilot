e25ed93d8cf55ec3a14c0bb884bc54b0d165891d
Josh de Leeuw <josh.deleeuw@gmail.com>
2017-04-11 14:24:07 -0400

    final changes before launching on prolific

--- js/serverComm.js
+++ js/serverComm.js
@@ -13,7 +13,7 @@ serverComm.register_subject = function(subject_id, callback_continue, callback_e
         console.log(response);
       }
       if(response.success){
-        if(response.exclude){
+        if(response.excluded){
           callback_exclude(response);
         } else {
           callback_continue(response);
